25a3f7c17f401ae3deda1f41a8ae1badaee09241
SERGESPA <SERGESPA@ALII-8T50ZN2A>
2025-07-31 16:47:59 -0600

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace ElectricalTest
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
